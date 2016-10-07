3fa8705f9ebbebd80e82e245fa79fbcb686e555e
Nattapat Sukpootanan <Nattapat@Nattapats-Macbook.local>
2016-10-07 19:44:07 +0700

    add template navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
+import Navbar from './Navbar'
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Navbar/>
       </div>
     )
   }
