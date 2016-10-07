a501d8c99d765461b64dab2e7429fe763da38047
Nattapat Sukpootanan <Nattapat@Nattapats-Macbook.local>
2016-10-07 19:15:15 +0700

    initial project

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
