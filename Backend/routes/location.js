3c77a45ed0175c203098aaaccf523c180e752cf7
PNNutkung <pipatpol.t@ku.th>
2016-10-08 17:06:01 +0700

    Add template files

--- Backend/routes/location.js
+++ Backend/routes/location.js
@@ -1,14 +1,15 @@
 (function () {
     'use strict';
-    module.exports = function(app, express) {
+    module.exports = function(app) {
         app.get('/location', function(req, res) {
             return res.json({message: 'Hello World'});
         });
 
-        app.post('/location', function(req, res){
-            const lat = req.body.lat;
-            const lon = req.body.lon;
-            console.log(lat,lon);
+        app.post('/location', function(req, res) {
+            return res.json({
+                lat: req.body.lat,
+                lon: req.body.lon
+            });
         });
     };
 }());
