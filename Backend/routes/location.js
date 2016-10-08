2c791c349a2efebccd554ee4ab013a3edf4007aa
PNNutkung <pipatpol.t@ku.th>
2016-10-08 21:47:45 +0700

    Change client server engaging

--- Backend/routes/location.js
+++ Backend/routes/location.js
@@ -1,15 +1,23 @@
-(function () {
+(function() {
     'use strict';
-    module.exports = function(app, net) {
-        app.post('/location', function(req, res) {
-            const client = net.connect({port: 5000});
+    module.exports = function(app) {
+        const serverListener = function(lat, lon) {
+            const net = require('net');
+            const client = new net.Socket();
+            client.connect(5000, 'localhost', function() {
+                client.write(`${lat},${lon}`);
+            });
+
             let suggestedData = {};
             client.on('data', function(data) {
-                suggestedData = data.toString();
-            });
-            client.on('end');
+                suggestedData = JSON.stringify(data.toString());
+                client.end();
 
-            return res.json(JSON.stringify(suggestedData));
+                return suggestedData;
+            });
+        };
+        app.post('/location', function(req, res) {
+            return res.json(serverListener(req.body.lat, req.body.lon));
         });
     };
 }());
