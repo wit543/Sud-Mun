6d8e69c26a87098d613b6175100626876dc18143
Nattapat Sukpootanan <Nattapat@Nattapats-Macbook.local>
2016-10-08 17:34:09 +0700

    edit searchbar

--- Frontend/src/components/App/SearchBar.js
+++ Frontend/src/components/App/SearchBar.js
@@ -1,35 +1,9 @@
 import React, { Component } from 'react'
 export default class App extends Component {
-  componentDidMount(){
-    this.createMap();
-  }
-  componentDidUpdate(){
-    if(this.lastLat == this.props.lat && this.lastLng == this.props.lng){
-    return;
-    }
-    this.lastLat = this.props.lat;
-		this.lastLng = this.props.lng
-
-		let map = new GMaps({
-			el: '#map',
-			lat: this.props.lat,
-			lng: this.props.lng
-		});
-
-
-		map.addMarker({
-			lat: this.props.lat,
-			lng: this.props.lng
-		});
-
-  }
   render() {
     return (
       <div>
-        <Navbar/>
-        <div className="map-holder">
-          <div id="map"></div>
-        </div>
+        <input type="text"/>
       </div>
     )
   }
