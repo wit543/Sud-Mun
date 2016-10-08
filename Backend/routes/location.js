8fb4c8050010e6f0f9fccf0e8f16a2a06441519c
PNNutkung <pipatpol.t@ku.th>
2016-10-08 12:04:55 +0700

    Add simple location api route

---
+++ Backend/routes/location.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    module.exports = function(app, express) {
+        app.get('/location', function(req, res) {
+            return res.json({
+                message: 'Hello World'
+            });
+        });
+    };
+}());
---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+    const express = require('express');
+    const app = express();
+    const bodyParser = require('body-parser');
+    const morgan = require('morgan');
+    const port = process.env.PORT || 3000;
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
+        next();
+    });
+    app.use(morgan('dev'));
+
+    app.listen(3000, function(req, res) {
+        console.log('Listening on port 3000...');
+    });
+    require('./routes/routesManager')(app, express);
+})();
