9afc60bca9395fc98e06b565229954133d59d02d
PNNutkung <pipatpol.t@ku.th>
2016-10-08 12:10:57 +0700

    Change port

--- Backend/routes/location.js
+++ Backend/routes/location.js
@@ -1,10 +1,8 @@
-(function() {
+(function () {
     'use strict';
     module.exports = function(app, express) {
         app.get('/location', function(req, res) {
-            return res.json({
-                message: 'Hello World'
-            });
+            return res.json({message: 'Hello World'});
         });
     };
 }());
