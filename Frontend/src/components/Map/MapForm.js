59bb0c800f0e93d9be2e95199073c19219c5cf00
gunhappy <gunhappy123@gmail.com>
2016-10-09 07:25:34 +0700

    modal information

--- Frontend/src/components/Map/MapForm.js
+++ Frontend/src/components/Map/MapForm.js
@@ -16,12 +16,67 @@ import { FormGroup ,
         return (
           <div className="text-center">
             <form className="MapForm">
-              <Row>
-                <Col md={5}>
+            <br/>
+              <Row className="row">
+                <Col md={4}>
                   <span className="icon"></span>
                 </Col>
-                <Col md={7}>
-                  <span className="title">Rice ข้าวที่แนะนำ</span>
+                <Col md={6}>
+                  <span className="title text">Rice ข้าวที่แนะนำ</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="type-head head">พันธุ์</span>
+                </Col>
+                <Col md={6}>
+                  <span className="type text">กข45</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="age-head head">อายุเก็บเกี่ยว</span>
+                </Col>
+                <Col md={6}>
+                  <span className="age text">ปลายพฤศจิกายน</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="area-head head">พ์้นที่ปลูก</span>
+                </Col>
+                <Col md={6}>
+                  <span className="area text">ลุ่ม น้ำลึกไม่เกิน50ซม</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="season-head head">ฤดูที่เหมาะสม</span>
+                </Col>
+                <Col md={6}>
+                  <span className="season text">นาปี</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="resist-head head">ต้านทานโรค</span>
+                </Col>
+                <Col md={6}>
+                  <span className="resist text">ขอบใบแห้ง</span>
+                </Col>
+              </Row>
+
+              <Row className="row">
+                <Col md={4}>
+                  <span className="enemy-head head">ศัตรูพืช</span>
+                </Col>
+                <Col md={6}>
+                  <span className="enemy text">หนอนกอ</span>
                 </Col>
               </Row>
 
